b62405cd9aacb68bd83c6cebda23d8b4b95e0b41
lalithasaga <132065591+lalithasaga@users.noreply.github.com>
2023-05-01 20:42:01 +0530

    Add files via upload

--- well.js
+++ well.js
@@ -17,6 +17,7 @@ function addItem(e) {
 
   var li = document.createElement('li');
   li.className = 'list-group-item';
+  li.id = allUserDetails.length - 1; // Set the ID of the list item
   var nameSpan = document.createElement('span');
   nameSpan.className = 'item-name';
   nameSpan.appendChild(document.createTextNode(itemName));
@@ -29,6 +30,15 @@ function addItem(e) {
   var deleteBtn = document.createElement('button');
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
   deleteBtn.appendChild(document.createTextNode('X'));
+  
+  // Add an event listener to the delete button to remove the item
+  deleteBtn.addEventListener('click', function() {
+    var index = parseInt(li.id);
+    allUserDetails.splice(index, 1);
+    localStorage.setItem('userDetails', JSON.stringify(allUserDetails));
+    li.remove();
+  });
+  
   var editBtn = document.createElement('button');
   editBtn.className = 'btn btn-primary btn-sm float-right edit';
   editBtn.appendChild(document.createTextNode('Edit'));
